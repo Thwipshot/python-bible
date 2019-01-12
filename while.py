f76b675212713a4501c7977fb85e88175321cdad
Thwipshot <ethumpert@gmail.com>
2019-01-11 20:05:12 -0600

    changed the while loop to create a list using user input

--- while.py
+++ while.py
@@ -1,6 +1,8 @@
-number = 1
+L = []
 
-while number <= 1500:
-    if number % 2 ==0:
-        print(number)
-    number = number + 1
+while len(L) < 3:
+    new_name = input("Please add a new name: ").strip().capitalize()
+    L.append(new_name)
+
+print("Sorry list is full")
+print(L)
