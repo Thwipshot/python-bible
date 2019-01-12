e1fab8613b21d2c5081fc6868e78c6892dc20dbc
Thwipshot <ethumpert@gmail.com>
2019-01-12 13:26:12 -0600

    more for loop practice using Python

--- for.py
+++ for.py
@@ -1,15 +1,11 @@
-vowels = 0
-consonants = 0
+students = {
+    "male": ["Tom", "Charlie", "Harry", "Frank"],
+    "female": [ "Sarah", "Huda", "Samantha", "Emily", "Elizabeth"]
+    }
 
-for letter in "supercalifragilisticexpialidocious":
-    if letter.lower() in "aeiou":
-        vowels = vowels + 1
-    elif letter == " ":
-        pass
-    else:
-        consonants = consonants + 1
-
-print("There are {} vowels".format(vowels))
-print("There are {} consonants".format(consonants))
+for key in students.keys():
+    for name in students[key]:
+        if "a" in name:
+            print(name)
     
 
