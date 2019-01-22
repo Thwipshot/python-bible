47e904478cb6350a5a0a677bc0c267c880a07051
Thwipshot <ethumpert@gmail.com>
2019-01-22 17:35:20 -0600

    added Methods and base class template for coins

--- coins.py
+++ coins.py
@@ -1,12 +1,66 @@
+import random
+
+class Coin:
+
+    def __init__(self, rare = False, clean = True):
+        self.is_rare = rare
+        self.is_clean = clean
+
+        if self.is_rare:
+            self.value = self.original_value * 1.25
+        else:
+            self.value = self.original_value
+
+        if self.clean:
+            self.colour = self.clean_colour
+        else:
+            self.colour = self.rusty_colour
+
+    def rust(self):
+        self.colour = self.rusty_colour
+
+    def clean(self)
+        self.colour = self.clean_colour
+
+    def __del__(self):
+        print("Coin spent!")
+
+    def flip(self):
+        heads_options = [True, False]
+        choice = random.choice(heads_options)
+        self.heads = choice
+
 class Pound:
 
-    value = 1.00
-    color = "gold"
-    num_edges = 1
-    diameter = 22.5 # mm
-    thickness = 3.15 # mm
-    heads = True
+    def __init__(self, rare=False):
+
+        self.rare = rare
+
+        if self.rare:
+            self.value = 1.25
+        else:
+            self.value = 1.00
+        
+        self.value = 1.00
+        self.color = "gold"
+        self.num_edges = 1
+        self.diameter = 22.5 # mm
+        self.thickness = 3.15 # mm
+        self.heads = True
+
+    def rust(self):
+        self.colour = "greenish"
+
+    def clean(self):
+        self.colour = "gold"
+
+    def flip(self):
+        heads_options = [True, False]
+        choice = random.choice(heads_options)
+        self.heads = choice
+
+    def __del__(self):
+        print("Coin spent!")
 
 
-coin1 = Pound()
-print(coin1.value)
+
